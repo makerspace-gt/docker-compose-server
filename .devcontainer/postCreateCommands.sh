8c59a5515ac4958cb471cfb476b92dda601e4f37
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2025-02-26 02:40:03 +0100

    test if these are (not) needed

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,11 +1,10 @@
 # Debian packages install
-export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get -y install ---no-install-recommends $(./.devcontainer/debian-packages.txt)
-rm -rf /var/lib/apt/lists/*
-rm -Rf /usr/share/doc && rm -Rf /usr/share/man
-apt-get clean
+# export DEBIAN_FRONTEND=noninteractive
+# apt-get update
+# apt-get -y install ---no-install-recommends $(./.devcontainer/debian-packages.txt)
+# rm -rf /var/lib/apt/lists/*
+# apt-get clean
 
 # pip and ansible-galaxy-install
-pip install -r ./.devcontainer/pip-requirements.txt
+# pip install -r ./.devcontainer/pip-requirements.txt
 ansible-galaxy install -r ./.devcontainer/ansible-galaxy-requirements.yml
